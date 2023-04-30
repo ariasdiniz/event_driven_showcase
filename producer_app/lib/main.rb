51e8cc431f156b5dfb14ed859f19ab95de798db6
Aria Diniz <aria.diniz.dev@gmail.com>
2023-04-30 12:08:04 -0300

    Adjusting Gemfile for new macaw versions

--- producer_app/lib/main.rb
+++ producer_app/lib/main.rb
@@ -5,11 +5,11 @@ require 'macaw_framework'
 require_relative 'usecase/process_new_user'
 
 macaw = MacawFramework::Macaw.new
-sqs = Aws::SQS::Client.new(region: 'sa-east-1', endpoint: 'http://172.18.0.2:4566')
+sqs = Aws::SQS::Client.new(region: 'sa-east-1', endpoint: 'http://172.18.0.3:4566')
 new_user = ProcessNewUser.new(sqs)
 
-macaw.post('create_new_user') do |headers, body, parameters|
-  new_user.process(body)
+macaw.post('create_new_user') do |context|
+  new_user.process(context[:body])
 end
 
 macaw.start!
